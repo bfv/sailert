0a0f733b8f16258d2cea4f9f3d91327f7ac37dfc
bfv <dev@bfv.io>
2017-01-27 15:06:39 +0100

    speed displayable in various units

--- app/components/speed/speed.component.ts
+++ app/components/speed/speed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './../../store/appstate';
 import { Observable } from 'rxjs';
@@ -10,25 +10,49 @@ import { ChangeDetectorRef } from '@angular/core';
     selector: 'speed',
     templateUrl: 'speed.component.html',
     styleUrls: ['../geo.css', 'speed.component.css'],
+    inputs: ['units']
 })
 export class SpeedComponent implements OnInit {
 
-    public speed: number = 5.3;
-    public speedReading: string;
+    private speed: number = 0.0;
+    public speedReading: string = '';
+
     public units: string = "kt";
-    private speedObservable: Observable<{}>;
+    public unitsReading: string;
+
+    private unitFactor: number;
+    public speedClasses: string;    // css classes for the speed field
 
     constructor(private store: Store<AppState>, private ref: ChangeDetectorRef) { }
 
     ngOnInit() {
 
-        this.speedObservable = this.store.select('speed');
+        let speedObservable = <Observable<number>>this.store.select('speed');
 
-        this.speedObservable
+        speedObservable
             .subscribe(v => {
-                this.speed = <number>v * (3600 / 1852);
-                this.speedReading = this.speed.toFixed(1).toString() + ' kt';
+
+                let unitFactor: number;
+                if (this.units == 'kmh') {
+                    unitFactor = 3.6;
+                    this.unitsReading = 'km/h';
+                }
+                else if (this.units = 'kt') {
+                    unitFactor = (3600 / 1852);
+                    this.unitsReading = 'kt';
+                }
+                else {
+                    this.units = 'ms';
+                    this.unitsReading = 'm/s';
+                    unitFactor = 1;
+                }
+                this.speed = v * unitFactor;
+                this.speedReading = this.speed.toFixed(1).toString();
                 this.ref.detectChanges();
             });
+
+        this.speedClasses = 'text ' + this.units;
+
     }
 }
+
