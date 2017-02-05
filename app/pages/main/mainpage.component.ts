68a5c45dfda35f02d1fdd96688d0f48f2f6d1328
bfv <dev@bfv.io>
2017-02-05 12:35:14 +0100

    #6 cleaned up code, applied transition

--- app/pages/main/mainpage.component.ts
+++ app/pages/main/mainpage.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 // nativescript
 import { RadSideDrawerComponent, SideDrawerType } from 'nativescript-telerik-ui/sidedrawer/angular';
-import { DrawerTransitionBase, SlideInOnTopTransition, PushTransition } from 'nativescript-telerik-ui/sidedrawer';
-import { Page } from "ui/page";
+import { DrawerTransitionBase, PushTransition } from 'nativescript-telerik-ui/sidedrawer';
 
 @Component({
     moduleId: module.id,
@@ -12,38 +11,27 @@ import { Page } from "ui/page";
     templateUrl: 'mainpage.component.html',
     styleUrls: ['mainpage.component.css'],
 })
-export class MainpageComponent implements OnInit, AfterViewInit {
+export class MainpageComponent implements OnInit {
 
-    @ViewChild('drawer') public drawer: RadSideDrawerComponent;
+    @ViewChild('drawerComponent') public drawerComponent: RadSideDrawerComponent;
+    private drawer: SideDrawerType;
+    private sideDrawerTransition: DrawerTransitionBase;
 
-    public sideDrawerTransition: DrawerTransitionBase;
+    constructor(private router: Router) {
 
-    constructor(private router: Router, private page: Page) {
-        this.page.on('loaded', this.onPageLoaded, this);
     }
 
     ngOnInit() {
+        this.drawer = this.drawerComponent.sideDrawer;
+        this.sideDrawerTransition = new PushTransition();
         this.router.events.subscribe((e) => {
             if (e instanceof NavigationEnd) {
-                this.drawer.sideDrawer.closeDrawer();
+                this.drawer.closeDrawer();
             }
         });
-
-    }
-
-    ngAfterViewInit() {
-
-    }
-
-    onPageLoaded() {
-        this.sideDrawerTransition = new PushTransition();
     }
 
     openMenu() {
-        console.log('open menu');
-        //this.router.navigate(['/main/settings']);
-        this.drawer.sideDrawer.toggleDrawerState();
+        this.drawer.toggleDrawerState();
     }
-
-
 }
