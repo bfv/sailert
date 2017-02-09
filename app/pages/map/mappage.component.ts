26f3f586c82d1d99d00523832983adc9417886c6
bfv <dev@bfv.io>
2017-02-09 19:13:20 +0100

    #17 added map page component skeleton

---
+++ app/pages/map/mappage.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'mappage',
+    templateUrl: 'mappage.component.html',
+    styleUrls: ['mappage.component.css']
+})
+export class MappageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
