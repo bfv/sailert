0e27383434153595914a703218ace3a15e7be5d2
bfv <dev@bfv.io>
2017-02-09 19:13:44 +0100

    #18 added waypoints page component

---
+++ app/pages/waypoints/waypointspage.components.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'waypointspage',
+    templateUrl: 'waypointspage.component.html',
+    styleUrls: ['waypointspage.component.css']
+})
+export class WaypointpageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
