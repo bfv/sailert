a4b00fafab03fe3f7d4c79115c1e97ad1a7a037f
bfv <dev@bfv.io>
2017-02-11 13:16:56 +0100

    #20 first draft version BottomNavbar

--- app/modules/bottomnavbar/components/bottom-navbar/bottom-navbar.component.ts
+++ app/modules/bottomnavbar/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, AfterViewInit, ContentChildren, OnDestroy } from '@angular/core';
 import { Page } from 'ui/page';
+import { BottomNavitemComponent } from './../bottom-navitem/bottom-navitem.component';
 
 @Component({
     moduleId: module.id,
@@ -7,11 +8,42 @@ import { Page } from 'ui/page';
     templateUrl: 'bottom-navbar.component.html',
     styleUrls: ['bottom-navbar.component.css']
 })
-export class BottomNavbarComponent implements OnInit {
+export class BottomNavbarComponent implements OnInit, AfterViewInit, OnDestroy {
+
+    // @ViewChildren decorator doesn't work when the first child is ng-content
+    @ContentChildren(BottomNavitemComponent) contentItems: QueryList<BottomNavitemComponent>;
+    navItems: BottomNavitemComponent[] = [];
+    private subs: any[] = [];
 
     constructor(private page: Page) { }
 
     ngOnInit() {
-        console.log('page defined: ', this.page != undefined);
+
+    }
+
+    ngOnDestroy() {
+        for (let item of this.navItems) {
+            item.onItemSelected.unsubscribe();
+        }
+    }
+
+    ngAfterViewInit() {
+
+        this.contentItems.forEach((item, id, allItems) => {
+            item.id = id;
+            this.navItems.push(item);
+            item.onItemSelected.subscribe(id => {
+                this.selectNavitem(id);
+            });
+        });
+
+        // first select the BottomNavitemComponent with index 0
+        this.selectNavitem(0);
+    }
+
+    public selectNavitem(selectedIndex: number) {
+        for (let item of this.navItems) {
+            item.setSelected(item.id == selectedIndex);
+        }
     }
 }
--- app/modules/bottomnavbar/components/bottom-navitem/bottom-navitem.component.ts
+++ app/modules/bottomnavbar/components/bottom-navitem/bottom-navitem.component.ts
@@ -1,35 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-//import { Router } from '@angular/router';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router/router-extensions';
+import { BottomNavbarComponent } from './../../components/bottom-navbar/bottom-navbar.component';
 
 @Component({
     moduleId: module.id,
     selector: 'bottom-navitem',
     templateUrl: 'bottom-navitem.component.html',
     styleUrls: ['bottom-navitem.component.css'],
-    inputs: ['icon', 'text', 'route']
+    inputs: ['icon', 'text', 'route'],
+    outputs: ['onItemSelected']
+    //changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BottomNavitemComponent implements OnInit {
 
+    id: number;
     icon: string;
     text: string;
     route: string;
 
-    constructor(private routerExtensions: RouterExtensions) { }
+    componentClass: string;
+    onItemSelected: EventEmitter<number>;
+
+    constructor(private router: Router, private ref: ChangeDetectorRef) {
+        //console.log('parent', JSON.stringify(parent));
+    }
 
     ngOnInit() {
 
+        if (!this.text || this.text == '') {
+            throw ('text property for bottom-navitem isn\'t set');
+        }
+
+        if (!this.icon || this.icon == '') {
+            throw ('icon property for bottom-navitem isn\'t set');
+        }
+
+        this.onItemSelected = new EventEmitter<number>();
     }
 
-    navigate() {
+    navigate(): void {
         if (this.route) {
-            this.routerExtensions.navigate([this.route], {
-                transition: {
-                    name: "slideBottom",
-                    duration: 2000,
-                    curve: "linear"
-                }
-            });
+            this.router.navigate([this.route]);
         }
+        this.onItemSelected.emit(this.id);
     }
+
+    setSelected(isSelected: boolean): void {
+        this.componentClass = isSelected ? 'bottom-navitem-selected' : 'bottom-navitem';
+        this.ref.detectChanges();
+    }
+
 }
