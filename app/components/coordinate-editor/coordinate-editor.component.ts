ab6bc8f3dedaa7585ea4988ec04579cec4ab952e
bfv <dev@bfv.io>
2017-02-26 11:59:11 +0100

    #24 WIP coordinate editor

--- app/components/coordinate-editor/coordinate-editor.component.ts
+++ app/components/coordinate-editor/coordinate-editor.component.ts
@@ -4,11 +4,29 @@ import { Component, OnInit } from '@angular/core';
     moduleId: module.id,
     selector: 'coordinate-editor',
     templateUrl: 'coordinate-editor.component.html',
-    styleUrls: ['coordinate-editor.component.css']
+    styleUrls: ['coordinate-editor.component.css'],
+    inputs: ['hemispereType']
 })
 export class CoordinateEditorComponent implements OnInit {
 
+    hemisphere: string = 'N';
+    degrees: number = 0;
+    minutes: number = 0;
+    seconds: number = 0;
+
+    degreesFormatted: string;
+    minutesFormatted: string;
+    secondsFormatted: string;
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.formatCoordinate();
+    }
+
+    formatCoordinate(): void {
+        this.degreesFormatted = this.degrees.toFixed(0) + '\u00B0';;
+        this.minutesFormatted = this.minutes.toFixed(0) + '\'';
+        this.secondsFormatted = this.seconds.toFixed(1) + '"';
+    }
 }
