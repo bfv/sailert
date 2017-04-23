0fabb5265ba6b70d30a36df3c3d1003baa23f4f3
bfv <dev@bfv.io>
2017-04-23 07:42:42 +0200

    #35 removed component editor

--- app/components/components.module.ts
+++ app/components/components.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { CoordinateEditorComponent } from './coordinate-editor/coordinate-editor.component';
-
 @NgModule({
-    declarations: [CoordinateEditorComponent],
-    exports: [CoordinateEditorComponent],
+    declarations: [],
+    exports: [],
     imports: [],
     providers: [],
 })
